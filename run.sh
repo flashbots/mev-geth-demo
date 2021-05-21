fcb44c4736189e8b7eefcd7698b0a89677dd19c9
Jason Paryani <github@jparyani.com>
2021-05-21 12:23:38 -0700

    Remove ws flags

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ rm -rf datadir
 
 $GETH init --datadir datadir genesis.json
 
-$GETH --datadir datadir --rpc --rpcapi debug,personal,eth,net,web3,txpool,admin,miner --miner.etherbase=0xd912aecb07e9f4e1ea8e6b4779e7fb6aa1c3e4d8 --miner.gasprice 0 --mine --miner.threads=8 --relaywsurl="ws://localhost:8080" --relaywssigningkeystorefile="./sample_keystore_file" --relaywskeystorepw="randomWalletPW"
+$GETH --datadir datadir --rpc --rpcapi debug,personal,eth,net,web3,txpool,admin,miner --miner.etherbase=0xd912aecb07e9f4e1ea8e6b4779e7fb6aa1c3e4d8 --miner.gasprice 0 --mine --miner.threads=8
